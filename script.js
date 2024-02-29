decbde92908d78eecc8911bf7132124e1b2e41fa
Dardo Leguizamon <dardoleguizamon@gmail.com>
2024-02-28 23:34:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-// Cerrar el menú desplegable al hacer clic en una categoría
 var navLinks = document.querySelectorAll('.navbar-nav li a');
 
 navLinks.forEach(function (link) {
   link.addEventListener('click', function () {
     var collapse = document.querySelector('.navbar-collapse');
     collapse.classList.remove('show');
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var backToTopButton = document.getElementById("back-to-top");
+
+  window.addEventListener("scroll", function () {
+    if (window.pageYOffset > 100) {
+      backToTopButton.style.display = "block";
+    } else {
+      backToTopButton.style.display = "none";
+    }
+  });
+
+  backToTopButton.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   });
 });
